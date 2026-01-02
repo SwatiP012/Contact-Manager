edb539627e92632210dcf8bb7dbc2a4ed439628c
swati patel <202251139@iiitvadodara.ac.in>
2026-01-02 23:52:22 +0530

    Fix Vercel build by locking Node version to 18

--- server/server.js
+++ server/server.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
 connectDB();
 
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 app.use('/api/contacts', require('./routes/contacts'));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   );
-// }
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+);
